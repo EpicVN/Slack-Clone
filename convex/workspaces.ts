c922bf5e88ceccfcf61054504dba8ad931f4060d
EpicVN <ledangkhoahi3@gmail.com>
2024-09-12 10:34:58 +0700

    Add toolbar component

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -1,6 +1,6 @@
-import { mutation, query } from './_generated/server';
-import { v } from 'convex/values';
 import { getAuthUserId } from '@convex-dev/auth/server';
+import { v } from 'convex/values';
+import { mutation, query } from './_generated/server';
 
 export const create = mutation({
   args: {
@@ -29,5 +29,18 @@ export const get = query({
   args: {},
   handler: async (ctx) => {
     return await ctx.db.query('workspaces').collect();
+  },
+});
+
+export const getById = query({
+  args: { id: v.id('workspaces') },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      throw new Error('Unauthorized');
+    }
+
+    return await ctx.db.get(args.id);
   },
 });
