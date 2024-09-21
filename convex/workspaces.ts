f3d736ffa130dd466c2d570ec4e12cfd35699e19
EpicVN <ledangkhoahi3@gmail.com>
2024-09-21 21:44:18 +0700

    Add join workspace with code function

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -78,6 +78,49 @@ export const create = mutation({
   },
 });
 
+export const join = mutation({
+  args: {
+    joinCode: v.string(),
+    workspaceId: v.id('workspaces'),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      throw new Error('Unauthorized');
+    }
+
+    const workspace = await ctx.db.get(args.workspaceId);
+
+    if (!workspace) {
+      throw new Error('Unauthorized');
+    }
+
+    if (workspace.joinCode !== args.joinCode.toLowerCase()) {
+      throw new Error('Invalid join code');
+    }
+
+    const existingMember = await ctx.db
+      .query('members')
+      .withIndex('by_workspace_id_user_id', (q) =>
+        q.eq('workspaceId', args.workspaceId).eq('userId', userId),
+      )
+      .unique();
+
+    if (existingMember) {
+      throw new Error('Already an active member of this workspace');
+    }
+
+    await ctx.db.insert('members', {
+      userId,
+      workspaceId: workspace._id,
+      role: 'member',
+    });
+
+    return workspace._id;
+  },
+});
+
 export const get = query({
   args: {},
   handler: async (ctx) => {
@@ -129,6 +172,31 @@ export const getById = query({
     }
 
     return await ctx.db.get(args.id);
+  },
+});
+
+export const getInfoById = query({
+  args: { id: v.id('workspaces') },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      return null;
+    }
+
+    const member = await ctx.db
+      .query('members')
+      .withIndex('by_workspace_id_user_id', (q) =>
+        q.eq('workspaceId', args.id).eq('userId', userId),
+      )
+      .unique();
+
+    const workspace = await ctx.db.get(args.id);
+
+    return {
+      name: workspace?.name,
+      isMember: !!member,
+    };
   },
 });
 
